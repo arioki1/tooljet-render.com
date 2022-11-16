cf2fdd2daee8f0a5f782b362eb931ec87394eea2
Yoga Setiawan <ariokidev@gmail.com>
2022-11-17 06:37:17 +0700

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #run this command only when first install tooljet
-npm run db:migrate:prod
-npm run db:seed:prod
+# npm run db:migrate:prod
+# npm run db:seed:prod
 
 npm run start:prod
